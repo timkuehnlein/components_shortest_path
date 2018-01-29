b4cfc526f3230ae20c0ae3d1aa7647639b45d95a
tim <kuehnleintim@gmail.com>
2018-01-29 11:38:11 +0100

    dijkstra algorithm and tests added

--- dijkstra/src/Component.java
+++ dijkstra/src/Component.java
@@ -1,2 +1,33 @@
-public class Component {
+import java.util.LinkedList;
+
+public class Component{
+
+    // static instance
+    private static Component instance = new Component();
+
+    // define port
+    public Port port;
+
+    public static Component getInstance() {
+        return instance;
+    }
+
+    private Component() {
+        port = new Port();
+    }
+
+    public String getVersion() {
+        return "Dijkstra 0.9";
+    }
+
+    public class Port implements IComponent{
+        public void printVersion() {
+            System.out.println(getVersion() + "\n");
+        }
+
+        @Override
+        public LinkedList<Vertex> getShortestPath(Vertex source, Vertex target) {
+            return null;
+        }
+    }
 }
--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,5 @@
 public class Application {
+    public static void main(String[] Args){
+        System.out.println("Hello World");
+    }
 }
