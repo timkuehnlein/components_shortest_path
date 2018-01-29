c01270900956db65a4e83d18542db82e089c672e
Adrian Dumke <AdrianDumke@web.de>
2018-01-29 14:10:38 +0100

    new idea

--- bellman-ford/src/IComponent.java
+++ bellman-ford/src/IComponent.java
@@ -1,7 +1,5 @@
 import java.util.Map;
 
 public interface IComponent {
-
-    Graph.CostPathPair<Integer> getShortestPath(Graph<Integer> graph, Graph.Vertex<Integer> start, Graph.Vertex<Integer> end);
-    Map<Graph.Vertex<Integer>, Graph.CostPathPair<Integer>> getShortestPaths(Graph<Integer> graph, Graph.Vertex<Integer> start);
+ public String getShortestPath(int source, int  adjacencymatrix[][]);
 }
--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,8 @@
 public class Application {
     public static void main(String[] Args){
-        System.out.println("Hello World");
+
+
+
+
     }
 }
