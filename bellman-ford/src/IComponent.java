e66339edf7c40c4d1f590e1c7563d1fb41879d05
Adrian Dumke <AdrianDumke@web.de>
2018-01-29 12:00:11 +0100

    Implementierung fertig, Interfaces fertig

--- bellman-ford/src/IComponent.java
+++ bellman-ford/src/IComponent.java
@@ -1,2 +1,7 @@
+import java.util.Map;
+
 public interface IComponent {
+
+    Graph.CostPathPair<Integer> getShortestPath(Graph<Integer> graph, Graph.Vertex<Integer> start, Graph.Vertex<Integer> end);
+    Map<Graph.Vertex<Integer>, Graph.CostPathPair<Integer>> getShortestPaths(Graph<Integer> graph, Graph.Vertex<Integer> start);
 }
